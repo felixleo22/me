53cf0fcc181a7e35fcc8acb398b752435accd241
felixleo22 <felixleo22@users.noreply.github.com>
2022-03-09 20:21:11 +0000

    deploy: 5a1230ca865eef52b1b88fa7db2704f90cdb0c59

---
+++ _nuxt/0a64cd4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{397:function(e,n,t){"use strict";t.r(n);var l={},c=t(70),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v(" test ")])}),[],!1,null,null,null);n.default=component.exports}}]);
